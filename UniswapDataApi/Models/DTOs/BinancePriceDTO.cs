6dbc12d15b6ba35d1106c5d2ea3c83c0d6f2eee9
Firebun <firebuncrypto@gmail.com>
2020-04-17 18:43:22 -0400

    Retrieve HEX latest price call

---
+++ UniswapDataApi/Models/DTOs/BinancePriceDTO.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace UniswapDataApi.Models.DTOs
+{
+    public class BinancePriceDTO
+    {
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty("price")]
+        public string Price { get; set; }
+    }
+}
---
+++ UniswapDataApi/Models/DTOs/CoinbasePriceDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace UniswapDataApi.Models.DTOs
+{
+    public class CoinbasePriceDTO
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("base")]
+        public string Base { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("amount")]
+        public string Amount { get; set; }
+    }
+}
